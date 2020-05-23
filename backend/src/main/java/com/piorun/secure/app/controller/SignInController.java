baf3e1aa26b6e37955f4a61afb2d877346ed6bfa
mgn5268 <maciej.piorun@tieto.com>
2020-05-23 11:24:17 +0200

    Check

--- backend/src/main/java/com/piorun/secure/app/controller/SignInController.java
+++ backend/src/main/java/com/piorun/secure/app/controller/SignInController.java
@@ -14,6 +14,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
+
 @RestController
 public class SignInController {
 
