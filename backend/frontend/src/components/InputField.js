3d85699384b10d5f5fa251173a0e118ead65508b
MPiorunn <maciej.piorun2@gmail.com>
2020-09-27 21:53:55 +0200

    Added cords handling for local IPs

--- backend/frontend/src/components/InputField.js
+++ backend/frontend/src/components/InputField.js
@@ -1,6 +1,24 @@
 import React from 'react';
+import axios from 'axios';
+
 
 class InputField extends React.Component {
+
+
+    componentDidMount() {
+        // Simple POST request with a JSON body using axios
+        // const article = { title: 'React POST Request Example' };
+        // http://localhost:4000/http://localhost:8080
+        axios.defaults.headers.get['Access-Control-Allow-Origin'] = 'http://localhost:3000' // for all requests
+        axios.get(`http://localhost:8080`)
+            .then(res => {
+                console.log(res);
+            })
+            .catch(err => {
+                console.log(err);
+            })
+    }
+
     render() {
         return (
             <div className="inputField">
--- backend/src/main/java/com/piorun/secure/app/controller/LoginController.java
+++ backend/src/main/java/com/piorun/secure/app/controller/LoginController.java
@@ -12,6 +12,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,6 +27,12 @@ public class LoginController {
     private final ParamsVerifier verifier;
     private final SaltRepository saltRepository;
     private final UserRepository userRepository;
+
+    @CrossOrigin(origins = "http://localhost:3000")
+    @GetMapping("/")
+    public ResponseEntity<String> hello() {
+        return new ResponseEntity<>("Hello world", HttpStatus.OK);
+    }
 
     public LoginController(ParamsVerifier verifier, SaltRepository saltRepository, UserRepository userRepository) {
         this.verifier = verifier;
